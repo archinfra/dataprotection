a4c02591a9667afced4a7188510fd05b64dee768
yuanyp8 <geniusgavin@163.com>
2026-04-08 12:28:50 +0800

    fix release artifact download filtering

--- scripts/install/modules/00-header.sh
+++ scripts/install/modules/00-header.sh
@@ -7,7 +7,7 @@
 set -Eeuo pipefail
 
 APP_NAME="data-protection-operator"
-APP_VERSION="${APP_VERSION:-0.1.2}"
+APP_VERSION="${APP_VERSION:-0.1.3}"
 WORKDIR="/tmp/${APP_NAME}-installer"
 IMAGE_DIR="${WORKDIR}/images"
 MANIFEST_DIR="${WORKDIR}/manifests"
